f7544ee4cf764dcca017f07614a6c5eaa050ec58
SafwanBackar <sbackar29@gmail.com>
2022-02-17 18:46:52 +0530

    day 1 of JS

---
+++ variable.js
@@ -0,0 +1,36 @@
+// assign string, boolean, undefined and null data types
+
+
+
+// let chappa;
+// let ganesh;
+// let aafrin;
+// let hari;
+
+
+// let ziyaf = "myrna";
+// let haneen = "disabled by aafrin";
+// let sonu = "mutual";
+// let rafeen = "mute";
+
+// Declare variables to store your first name, last name, marital status, country and age in multiple lines
+
+// let firstName = "Safwan "
+// let lastName = "Backar"
+// let maritalStatus = true
+// let country = "India"
+// let age = 23
+// fullName = firstName + lastName;
+
+
+// Declare variables to store your first name, last name, marital status, country and age in a single line
+
+// let firstName = "Safwan", lastName = "Backar", maritalStatus = "Is Married", country = "India", age = "23";
+
+
+
+// Declare two variables myAge and yourAge and assign them initial values and log to the browser console.
+
+let myAge = 23;
+let yourAge = 46;
+
